700e71dec11f14222005e90c9dff4c2d5bb61110
Dmytro <baristin43@gmail.com>
2023-05-22 16:21:03 +0300

    Initial commit.

---
+++ app/src/main/java/it_school/sumdu/edu/myreminder/MainActivity.java
@@ -0,0 +1,14 @@
+package it_school.sumdu.edu.myreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
